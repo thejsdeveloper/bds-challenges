2316fc9a01db3b0c8317097a356d2c6ac44c8302
vikas yadav <thejsdeveloper@gmail.com>
2024-02-15 14:36:36 +0530

    feat: challenge 4

---
+++ src/app/components/Map/Map.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+function Map() {
+  return (
+    <svg
+      viewBox="0 0 2408 1569"
+      width={"100%"}
+      height={"100%"}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M230.5 8L533.5 1336.5M771 8L1195.5 1571.5M11 1483L2392.5 788.5"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M0.5 972.5L427.5 879L741 892.701M1011 904.5L1357 364M1011 904.5L1708.25 582.5M1011 904.5L741 892.701M2405.5 260.5L1708.25 582.5M0.5 333.5L334 487.5L556.5 387.584M1357 364L839.5 260.5L698 324.042M1357 364L1599.5 10.5M352 1384.5L381 1478M410 1571.5L381 1478M703 1283.5L741 1417L672.5 1493L381 1478M1599.5 1020.5L1650 1187.5L1561.5 1258M1561.5 1258L1135 1364M1561.5 1258L1727.75 1384.5M1973.5 1571.5L1806.5 1444.43M1727.75 1384.5L1829.5 1283.5M1727.75 1384.5L1806.5 1444.43M1806.5 1444.43L1662.5 1543.5M1708.25 582.5L1753.5 785.5L1650 879L1276.5 972.5L1316.5 1096.5M556.5 387.584L604.5 568.5L511 692.5L367 629M556.5 387.584L698 324.042M698 324.042L753.5 573.5M753.5 573.5L910 533M753.5 573.5L703 710L741 892.701"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M516 690L700.5 712.5"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M135.168 397L106.211 541.378L164.608 638.496L362 624.993L230.245 746H1"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M602 5L415 101L624.5 356.5"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1473.5 205L1675.5 366.5M1675.5 366.5L1905 177L2072 407M1675.5 366.5L1539 558.5L1263.5 520.5M1675.5 366.5C2174.7 726.1 1938.83 792.333 1758.5 780.5M1372.5 740.5C1339.67 768.167 1339.5 907 1430.5 866.5C1538.13 818.597 1572 798.5 1572 740.5V649.5"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M626.022 889C608.273 931.674 575.761 981.723 643.439 1009.96C695.448 1031.67 734.34 996.628 744.458 993.49L839.506 1083.86L1051 1036.32M246.328 1210L173.674 938.891L0 1107.39"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M245.5 1212.5L154.75 1269.5M64 1326.5L154.75 1269.5M154.75 1269.5L99 1170"
+        stroke="#E9E3E3"
+        stroke-width="12"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default Map;
