5bceed8dc22b8292862acba6188b73b8e3167746
vikas yadav <thejsdeveloper@gmail.com>
2024-03-10 19:30:19 +0530

    fix animation fix

--- src/app/challenges/solutions/Playlist.tsx
+++ src/app/challenges/solutions/Playlist.tsx
@@ -99,75 +99,73 @@ const Player = ({ track }: { track: Track }) => {
   return (
     <BottomSheet onToggle={() => setShowDetails((prev) => !prev)}>
       <div className="w-full h-full rounded-t-3xl bg-white">
-        <AnimatePresence mode="sync">
-          {!showDetails ? (
-            <div className="flex items-center gap-4 text-black p-4">
-              <motion.div layoutId={track.id}>
-                <Image
-                  src={track.cover}
-                  alt={track.title}
-                  width={100}
-                  height={100}
-                  className="rounded-xl size-14 object-cover"
-                />
+        {!showDetails ? (
+          <div className="flex items-center gap-4 text-black p-4">
+            <motion.div layoutId={track.id}>
+              <Image
+                src={track.cover}
+                alt={track.title}
+                width={100}
+                height={100}
+                className="rounded-xl size-14 object-cover"
+              />
+            </motion.div>
+            <motion.div className="flex-1 space-y-1">
+              <p className="text-sm">{track.title}</p>
+              <p className="text-xs text-gray-300">{track.artist}</p>
+            </motion.div>
+            <IoPlay className="size-10 fill-violet-500" />
+          </div>
+        ) : (
+          <div className="flex flex-col p-6 gap-4 h-full">
+            <motion.div layoutId={track.id} className="w-full h-2/3">
+              <Image
+                src={track.cover}
+                alt={track.title}
+                width={500}
+                height={500}
+                className="rounded-xl w-full h-full object-cover"
+              />
+            </motion.div>
+            <div className="flex items-center">
+              <motion.div className="space-y-1 flex-1 ">
+                <p className="text-base">{track.title}</p>
+                <p className="text-xs text-gray-500">{track.artist}</p>
               </motion.div>
-              <motion.div className="flex-1 space-y-1">
-                <p className="text-sm">{track.title}</p>
-                <p className="text-xs text-gray-300">{track.artist}</p>
-              </motion.div>
-              <IoPlay className="size-10 fill-violet-500" />
+              <LikeIcon size={24} />
             </div>
-          ) : (
-            <div className="flex flex-col p-6 gap-4 h-full">
-              <motion.div layoutId={track.id} className="w-full h-2/3">
-                <Image
-                  src={track.cover}
-                  alt={track.title}
-                  width={500}
-                  height={500}
-                  className="rounded-xl w-full h-full object-cover"
-                />
-              </motion.div>
-              <div className="flex items-center">
-                <motion.div className="space-y-1 flex-1 ">
-                  <p className="text-base">{track.title}</p>
-                  <p className="text-xs text-gray-500">{track.artist}</p>
-                </motion.div>
-                <LikeIcon size={24} />
-              </div>
 
-              <div className="flex flex-col gap-2 flex-1">
-                <ProgressBar
-                  className="flex flex-row items-center gap-3 text-white flex-1"
-                  value={50}
-                  minValue={0}
-                  maxValue={100}
-                >
-                  {({ percentage }) => (
-                    <>
-                      <div className="h-2 w-full bg-violet-300 relative rounded">
-                        <div
-                          className="absolute h-2 top-[50%] transform translate-y-[-50%] rounded bg-violet-500"
-                          style={{ width: percentage + "%" }}
-                        />
-                      </div>
-                    </>
-                  )}
-                </ProgressBar>
-                <div className="flex items-center justify-between text-xs text-gray-500">
-                  <p>2:42</p>
-                  <p>{track.duration}</p>
-                </div>
-              </div>
-
-              <div className="flex gap-4 justify-center items-center mt-auto">
-                <FaBackward className="size-6 fill-violet-300" />
-                <IoPlay className="size-10 fill-violet-400" />
-                <FaForward className="size-6 fill-violet-300" />
+            <div className="flex flex-col gap-2 flex-1">
+              <ProgressBar
+                className="flex flex-row items-center gap-3 text-white flex-1"
+                value={50}
+                minValue={0}
+                maxValue={100}
+              >
+                {({ percentage }) => (
+                  <>
+                    <div className="h-2 w-full bg-violet-300 relative rounded">
+                      <div
+                        className="absolute h-2 top-[50%] transform translate-y-[-50%] rounded bg-violet-500"
+                        style={{ width: percentage + "%" }}
+                      />
+                    </div>
+                  </>
+                )}
+              </ProgressBar>
+              <div className="flex items-center justify-between text-xs text-gray-500">
+                <p>2:42</p>
+                <p>{track.duration}</p>
               </div>
             </div>
-          )}
-        </AnimatePresence>
+
+            <div className="flex gap-4 justify-center items-center mt-auto">
+              <FaBackward className="size-6 fill-violet-300" />
+              <IoPlay className="size-10 fill-violet-400" />
+              <FaForward className="size-6 fill-violet-300" />
+            </div>
+          </div>
+        )}
       </div>
     </BottomSheet>
   );
@@ -188,22 +186,24 @@ const BottomSheet = ({ children, onToggle }: BottomSheetProps) => {
   };
 
   return (
-    <div
-      className={cn(
-        "fixed flex items-center",
-        isOpen && "bg-white/10 backdrop-blur-sm  inset-0 z-10"
-      )}
-    >
-      <motion.div
-        animate={isOpen ? "open" : "closed"}
-        variants={variants}
-        transition={{ type: "spring", stiffness: 200, damping: 30 }}
-        className="fixed bottom-0 left-0 right-0 h-3/4 "
-        onClick={handleOpen}
+    <AnimatePresence mode="sync" initial={false}>
+      <div
+        className={cn(
+          "fixed flex items-center",
+          isOpen && "bg-white/10 backdrop-blur-sm  inset-0 z-10"
+        )}
       >
-        <div className="w-16 h-2 bg-gray-200 hover:bg-white rounded mx-auto absolute -top-4 left-1/2 -translate-x-1/2 cursor-grab" />
-        {children}
-      </motion.div>
-    </div>
+        <motion.div
+          animate={isOpen ? "open" : "closed"}
+          variants={variants}
+          transition={{ type: "spring", stiffness: 200, damping: 30 }}
+          className="fixed bottom-0 left-0 right-0 h-3/4 "
+          onClick={handleOpen}
+        >
+          <div className="w-16 h-2 bg-gray-200 hover:bg-white rounded mx-auto absolute -top-4 left-1/2 -translate-x-1/2 cursor-grab" />
+          {children}
+        </motion.div>
+      </div>
+    </AnimatePresence>
   );
 };
